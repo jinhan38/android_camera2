027c778d9100acaadc62ed7b62528bc187bdbc72
jinhan_mac <jinhan38@naver.com>
2023-06-30 18:22:11 +0900

    갤럭시에서 frame 수정 성공, 워치는 아직 안 됨

--- app/src/main/java/com/example/android_camera2/MainActivity.kt
+++ app/src/main/java/com/example/android_camera2/MainActivity.kt
@@ -1,75 +1,62 @@
 package com.example.android_camera2
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CameraMetadata
 import android.hardware.camera2.CaptureRequest
-import android.hardware.camera2.TotalCaptureResult
+import android.hardware.camera2.params.OutputConfiguration
 import android.hardware.camera2.params.SessionConfiguration
 import android.hardware.camera2.params.StreamConfigurationMap
-import android.media.Image
-import android.media.ImageReader
 import android.media.MediaRecorder
-import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
+import android.util.Range
 import android.util.Size
-import android.util.SparseArray
 import android.view.Surface
 import android.view.TextureView
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.util.concurrent.HandlerExecutor
 import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
 import java.io.IOException
-import java.io.OutputStream
-import java.util.Arrays
-import java.util.UUID
+import kotlin.concurrent.timer
 
 
+@SuppressLint("NewApi")
 class MainActivity : ComponentActivity() {
 
     private lateinit var btnCapture: Button
     private lateinit var textureView: TextureView
-    var ORIENTATION = SparseArray<Int>()
+    private var orientation = arrayListOf<Int>()
 
-    private var cameraId = ""
     private var cameraDevice: CameraDevice? = null
     private lateinit var cameraCaptureSession: CameraCaptureSession
     private lateinit var captureRequestBuilder: CaptureRequest.Builder
     private lateinit var imageDimension: Size
-    private lateinit var imageReader: ImageReader
 
     private lateinit var file: File
-    private val REQUEST_CAMERA_PERMISSION = 200
-    private var mFlashSupported = false
     private var mBackgroundHandler: Handler? = null
     private var mBackgroundThread: HandlerThread? = null
 
     var mIsRecordingVideo: Boolean = false
 
-    /**
-     * MediaRecorder
-     */
     private var mMediaRecorder: MediaRecorder? = null
 
     companion object {
         private const val TAG = "MainActivity"
+        private val REQUEST_CAMERA_PERMISSION = 200
     }
 
     private val cameraStateCallback = object : CameraDevice.StateCallback() {
@@ -107,10 +94,10 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        ORIENTATION.append(Surface.ROTATION_0, 90)
-        ORIENTATION.append(Surface.ROTATION_90, 0)
-        ORIENTATION.append(Surface.ROTATION_180, 270)
-        ORIENTATION.append(Surface.ROTATION_270, 180)
+        orientation.add(Surface.ROTATION_0, 90)
+        orientation.add(Surface.ROTATION_90, 0)
+        orientation.add(Surface.ROTATION_180, 270)
+        orientation.add(Surface.ROTATION_270, 180)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -124,117 +111,11 @@ class MainActivity : ComponentActivity() {
             if (btnCapture.text == "stop") {
                 stopRecordingVideo()
             } else {
-
                 startRecordingVideo()
             }
-//            takePicture()
         }
 
         mMediaRecorder = MediaRecorder()
-    }
-
-    private fun takePicture() {
-        if (cameraDevice == null) return
-        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        try {
-            val characteristics: CameraCharacteristics =
-                manager.getCameraCharacteristics(cameraDevice!!.id)
-
-            val jpegSize: Array<Size>? =
-                characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-                    ?.getOutputSizes(ImageFormat.JPEG)
-
-            var width = 640
-            var height = 480
-
-            if (!jpegSize.isNullOrEmpty()) {
-                width = jpegSize[0].width
-                height = jpegSize[0].height
-            }
-
-            val reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1)
-            val outputSurface = ArrayList<Surface>()
-            outputSurface.add(reader.surface)
-            outputSurface.add(Surface(textureView.surfaceTexture))
-
-
-            val captureBuilder: CaptureRequest.Builder =
-                cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
-            captureBuilder.addTarget(reader.surface)
-            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
-
-            val rotation = windowManager.defaultDisplay.rotation
-            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATION.get(rotation))
-
-            val path = "/sdcard/download/${UUID.randomUUID()}.jpg"
-//            val path = "${Environment.getExternalStorageDirectory()}/${UUID.randomUUID()}.jpg"
-            file = File(path)
-            val readerListener = ImageReader.OnImageAvailableListener {
-
-                var image: Image? = null
-
-                try {
-
-                    image = reader.acquireLatestImage()
-                    val buffer = image.planes[0].buffer
-                    val byte = ByteArray(buffer.capacity())
-                    buffer.get(byte)
-                    Log.d(TAG, "takePicture: byte : $byte")
-                    save(byte)
-                } catch (e: FileNotFoundException) {
-                    Log.e(TAG, "takePicture: FileNotFoundException ", e)
-                    e.printStackTrace()
-                } catch (e: IOException) {
-                    Log.e(TAG, "takePicture: IOException ", e)
-                    e.printStackTrace()
-                } finally {
-                    image?.close()
-                }
-
-            }
-
-
-            reader.setOnImageAvailableListener(readerListener, mBackgroundHandler)
-            val captureListener = object : CameraCaptureSession.CaptureCallback() {
-
-                override fun onCaptureCompleted(
-                    session: CameraCaptureSession,
-                    request: CaptureRequest,
-                    result: TotalCaptureResult
-                ) {
-                    super.onCaptureCompleted(session, request, result)
-                    Toast.makeText(this@MainActivity, "save : $file", Toast.LENGTH_SHORT).show()
-                    createCameraPreview()
-                }
-
-            }
-
-            val stateListener = object : CameraCaptureSession.StateCallback() {
-                override fun onConfigured(session: CameraCaptureSession) {
-                    try {
-                        cameraCaptureSession = session
-                        cameraCaptureSession.capture(
-                            captureBuilder.build(),
-                            captureListener,
-                            mBackgroundHandler
-                        )
-                    } catch (e: CameraAccessException) {
-                        Log.e(TAG, "onConfigured: e", e)
-                    }
-                }
-
-                override fun onConfigureFailed(session: CameraCaptureSession) {
-                    Log.d(TAG, "onConfigureFailed: session : $session")
-                }
-
-            }
-            cameraDevice?.createCaptureSession(outputSurface, stateListener, mBackgroundHandler)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-            Log.e(TAG, "takePicture: CameraAccessException : ", e)
-
-        }
-
     }
 
     private fun createCameraPreview() {
@@ -246,6 +127,9 @@ class MainActivity : ComponentActivity() {
             val surface = Surface(texture)
             captureRequestBuilder =
                 cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+//            [15, 15], [24, 24], [7, 30], [30, 30], [7, 60], [60, 60]
+            val range: Range<Int> = Range.create(30, 30)
+            captureRequestBuilder.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, range)
             captureRequestBuilder.addTarget(surface)
 
             val stateCallback = object : CameraCaptureSession.StateCallback() {
@@ -260,7 +144,14 @@ class MainActivity : ComponentActivity() {
                 }
 
             }
-            cameraDevice!!.createCaptureSession(listOf(surface), stateCallback, null)
+            val sessionConfiguration = SessionConfiguration(
+                SessionConfiguration.SESSION_REGULAR, listOf(
+                    OutputConfiguration(
+                        surface
+                    )
+                ), HandlerExecutor(mBackgroundThread!!.looper), stateCallback
+            )
+            cameraDevice!!.createCaptureSession(sessionConfiguration)
         } catch (e: CameraAccessException) {
             Log.e(TAG, "createCameraPreview: CameraAccessException : ", e)
         }
@@ -270,35 +161,20 @@ class MainActivity : ComponentActivity() {
         if (cameraDevice == null) {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
         }
-        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO)
+
+//        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO)
+
+        val range: Range<Int> = Range.create(30, 30)
+        captureRequestBuilder.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, range)
         try {
             cameraCaptureSession.setRepeatingRequest(
                 captureRequestBuilder.build(),
                 null,
                 mBackgroundHandler
             )
-
         } catch (e: CameraAccessException) {
             e.printStackTrace()
-
         }
-    }
-
-    private fun save(byte: ByteArray) {
-
-        var outPutStream: OutputStream? = null
-        try {
-            outPutStream = FileOutputStream(file)
-            outPutStream.write(byte)
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e(TAG, "save: e :", e)
-        } finally {
-            outPutStream?.close()
-        }
-
-
     }
 
     private fun openCamera() {
@@ -374,33 +250,47 @@ class MainActivity : ComponentActivity() {
                 cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_RECORD)
             val surfaces: MutableList<Surface> = ArrayList()
 
-            // Set up Surface for the camera preview
             val previewSurface = Surface(texture)
             surfaces.add(previewSurface)
             captureRequestBuilder.addTarget(previewSurface)
 
-            // Set up Surface for the MediaRecorder
             val recorderSurface = mMediaRecorder!!.surface
             surfaces.add(recorderSurface)
             captureRequestBuilder.addTarget(recorderSurface)
 
-            // Start a capture session
-            // Once the session starts, we can update the UI and start recording
-            cameraDevice!!.createCaptureSession(
-                surfaces,
+            val range: Range<Int> = Range.create(40, 50)
+            captureRequestBuilder.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, range)
+
+            val configurations = arrayListOf<OutputConfiguration>()
+
+            for (surface in surfaces) {
+                configurations.add(OutputConfiguration(surface))
+            }
+
+            val sessionConfiguration = SessionConfiguration(
+                SessionConfiguration.SESSION_REGULAR,
+                configurations.toList(),
+                HandlerExecutor(mBackgroundThread!!.looper),
                 object : CameraCaptureSession.StateCallback() {
                     override fun onConfigured(session: CameraCaptureSession) {
 
                         cameraCaptureSession = session
                         updatePreview()
-                        runOnUiThread(Runnable { // UI
+                        runOnUiThread {
                             mIsRecordingVideo = true
 
-                            // Start recording
                             mMediaRecorder!!.start()
 
                             btnCapture.text = "stop"
-                        })
+
+                            timer(initialDelay = 31000, period = 3000) {
+                                if (mIsRecordingVideo) {
+                                    this.cancel()
+                                    stopRecordingVideo()
+                                }
+
+                            }
+                        }
 
                         Toast.makeText(this@MainActivity, "촬영 시작", Toast.LENGTH_SHORT).show()
                     }
@@ -408,9 +298,11 @@ class MainActivity : ComponentActivity() {
                     override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
                         Toast.makeText(this@MainActivity, "Failed", Toast.LENGTH_SHORT).show()
                     }
-                },
-                mBackgroundHandler
+                }
             )
+
+            cameraDevice!!.createCaptureSession(sessionConfiguration)
+
         } catch (e: CameraAccessException) {
             e.printStackTrace()
             Log.e(TAG, "startRecordingVideo: CameraAccessException e", e)
@@ -428,25 +320,36 @@ class MainActivity : ComponentActivity() {
     @Throws(IOException::class)
     private fun setUpMediaRecorder() {
         mMediaRecorder!!.apply {
+            setMaxDuration(30000)
+
             setAudioSource(MediaRecorder.AudioSource.MIC)
             setVideoSource(MediaRecorder.VideoSource.SURFACE)
             setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            val path = File("/sdcard/download/ihpRecord")
+
+            // 워치에서 사용 가능한 경로
+            val path = File("${Environment.getExternalStorageDirectory()}/ihpRecord")
+
+            // 갤럭시에서 사용 가능한 경로
+//            val path = File("/sdcard/download/ihpRecord")
+
             if (!path.exists()) {
                 path.mkdirs()
             }
             val fileName = String.format("%d.mp4", System.currentTimeMillis())
             file = File(path, fileName)
 
+            Log.d(TAG, "setUpMediaRecorder: filePath : ${file.path}")
+
             setOutputFile(file.absolutePath)
             setVideoEncodingBitRate(10000000)
-            setVideoFrameRate(30)
+//            setVideoFrameRate(24)
+//            setCaptureRate(24.0)
             setVideoSize(imageDimension.width, imageDimension.height)
             setVideoEncoder(MediaRecorder.VideoEncoder.H264)
             setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
 
-            val rotation = windowManager.defaultDisplay.rotation
-            setOrientationHint(ORIENTATION.get(rotation))
+            val rotation = this@MainActivity.display?.rotation ?: 0
+            setOrientationHint(orientation[rotation])
             prepare()
         }
 
@@ -457,13 +360,8 @@ class MainActivity : ComponentActivity() {
         try {
             mIsRecordingVideo = false
             btnCapture.text = "Record"
-            // Stop recording
             mMediaRecorder!!.stop()
             mMediaRecorder!!.reset()
-            Toast.makeText(
-                this@MainActivity, "Video saved: ${file.path}",
-                Toast.LENGTH_SHORT
-            ).show()
             updatePreview()
         } catch (e: CameraAccessException) {
             Log.e(TAG, "stopRecordingVideo: eeee : ", e)
@@ -481,10 +379,8 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun startBackgroundThread() {
-
         mBackgroundThread = HandlerThread("Camera Background")
         mBackgroundThread!!.start()
-
         mBackgroundHandler = Handler(mBackgroundThread!!.looper)
 
     }
